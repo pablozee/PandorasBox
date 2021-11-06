a08b1e2c4dbf46718fa6fb4c0b84d58d2cff2b70
Zeshan Rasul <zeshanrasul@gmail.com>
2021-11-06 21:45:23 +0000

    WinMain

---
+++ PandorasBox/WinMain.cpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <Windows.h>
+#include "Application.h"
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	Application* app = new Application();
+	app->Run();
+	delete app;
+}
